5b8acf98c01550de2bb8fc1381692db80541fa93
briecodes <hello@brie.work>
2018-05-04 09:27:32 -0400

    practice routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :employees
+  
+  
+  # get '/dogs', to: 'dogs#index'
+  # get '/dogs/new', to: 'dogs#new'
+  # get '/dogs/:id', to: 'dogs#show'
+  # get '/dogs/:id/edit', to: 'dogs#edit'
+  # put '/dogs/:id', to: 'dogs#update'
+
+
   # get '/dogs/sorted', to: 'dogs#index_sorted', as: '/sorted/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
